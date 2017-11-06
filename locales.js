5357f97336ee42130dfc8a78683bae5cfe87eaf5
Devan Beitel <devan@devanb.us>
2017-11-06 12:42:54 -0500

    Adds en locale.

---
+++ locales.js
@@ -0,0 +1,88 @@
+{
+  en: {
+    account: {
+      'confirm-password': 'Confirm Password',
+      'create-account-button': 'Create Account',
+      'create-an-account': 'Create an Account',
+      'create-account-question': 'Need to create an account?',
+      'email': 'Email',
+      'email-example': 'email@example.com',
+      'forgot-password': 'Forgot Password',
+      'my-account': 'My Account',
+      'name': 'Name',
+      'password': 'Password',
+      'sign-in': 'Sign In',
+      'sign-out': 'Sign Out',
+      'update-password': 'Update Password'
+    },
+    hours: {
+      'closed': 'Closed',
+      'hours': 'Hours',
+      'limited': 'Limited Hours',
+      'normal': 'Normal Hours',
+      'open-today': 'Open Today',
+      'times': 'Times'
+    },
+    tabs: {
+      'account': 'Account',
+      'map': 'Map',
+      'report': 'Report',
+      'whats-open-question': 'What\'s Open?'
+    },
+    'cancel': 'Cancel',
+    'closes': 'Closes',
+    'comments': 'Comments',
+    'locations': 'Locations',
+    'nearby-locations': 'Nearby Locations',
+    'opens': 'Opens',
+    'recent-comments': 'Recent Comments',
+    'report': 'Report',
+    'report-create-account': 'To report a business, please create an account and/or sign in.',
+    'save': 'Save',
+    'status': 'Status'
+    'whats-open': 'What\'s Open'
+  },
+  de: {
+    account: {
+      'confirm-password': 'Confirm Password',
+      'create-account-button': 'Create Account',
+      'create-an-account': 'Create an Account',
+      'create-account-question': 'Need to create an account?',
+      'email': 'Email',
+      'email-example': 'email@example.com',
+      'forgot-password': 'Forgot Password',
+      'my-account': 'My Account',
+      'name': 'Name',
+      'password': 'Password',
+      'sign-in': 'Sign In',
+      'sign-out': 'Sign Out',
+      'update-password': 'Update Password'
+    },
+    hours: {
+      'closed': 'Closed',
+      'hours': 'Hours',
+      'limited': 'Limited Hours',
+      'normal': 'Normal Hours',
+      'open-today': 'Open Today',
+      'times': 'Times'
+    },
+    tabs: {
+      'account': 'Account',
+      'map': 'Map',
+      'report': 'Report',
+      'whats-open-question': 'What\'s Open?'
+    },
+    'cancel': 'Cancel',
+    'closes': 'Closes',
+    'comments': 'Comments',
+    'locations': 'Locations',
+    'nearby-locations': 'Nearby Locations',
+    'opens': 'Opens',
+    'recent-comments': 'Recent Comments',
+    'report': 'Report',
+    'report-create-account': 'To report a business, please create an account and/or sign in.',
+    'save': 'Save',
+    'status': 'Status'
+    'whats-open': 'What\'s Open'
+  }
+}
