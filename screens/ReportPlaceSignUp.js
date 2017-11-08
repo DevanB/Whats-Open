8c6b3618f41e63342f19fcd163ae4683f134d3f1
John Wilson <john@j-wilson.com>
2017-11-08 12:00:07 -0500

    Style the reportplacesignup screen

--- screens/ReportPlaceSignUp.js
+++ screens/ReportPlaceSignUp.js
@@ -12,9 +12,9 @@ import {
   StyleSheet,
   Text,
   TextInput,
-  TouchableWithoutFeedback,
   View
 } from 'react-native';
+import Touchable from 'react-native-platform-touchable';
 import { MapView } from 'expo';
 import Marker from '../components/Marker';
 import StyledTextInput from '../components/StyledTextInput';
@@ -36,17 +36,33 @@ export default class ReportPlace extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.reportSignUpText}>To report a business, please create an account and/or sign in.</Text>
-        <TouchableWithoutFeedback onPress={() => this.props.navigate('Account')}>
-          <View>
-            <Text>Create an Account</Text>
-          </View>
-        </TouchableWithoutFeedback>
+        <Touchable style={styles.button} onPress={() => this.props.navigation.navigate('Account')}>
+          <Text style={styles.buttonText}>Create an Account</Text>
+        </Touchable>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#0099ff',
+    borderRadius: 4,
+    marginBottom: 40,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 13,
+    paddingBottom: 12,
+    paddingTop: 12,
+    width: 170
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: '500',
+    letterSpacing: -0.3,
+    textAlign: 'center'
+  },
   container: {
     alignItems: 'center',
     display: 'flex',
@@ -57,6 +73,8 @@ const styles = StyleSheet.create({
     color: 'rgb(143, 142, 148)',
     fontSize: 17,
     letterSpacing: -0.4,
-    marginBottom: 16
+    marginBottom: 16,
+    textAlign: 'center',
+    width: 300
   }
 });
